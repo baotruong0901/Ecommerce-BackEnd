7d3cb0bf088ff0c648483264057f3484e8d0a095
bao truong <truongquangbaolut0901@gmail.com>
2023-06-01 18:20:01 +0700

    9 commit

--- src/routes/brandRoute.js
+++ src/routes/brandRoute.js
@@ -4,10 +4,10 @@ const brandControllers = require('../controllers/brandControllers')
 const { authMiddleware, isAdmin } = require("../middewares/authMiddleware")
 const { uploadPhoto, productImgResize } = require('../middewares/uploadImg')
 
-router.post('/brand', authMiddleware, isAdmin, uploadPhoto.array('image', 1), productImgResize, brandControllers.createBrand)
+router.post('/brand', authMiddleware, isAdmin, uploadPhoto.array('image', 1), brandControllers.createBrand)
 router.post('/brands', authMiddleware, isAdmin, brandControllers.createArrayBrand)
 router.get('/brands', brandControllers.getAllBrand)
-router.put('/brand', authMiddleware, isAdmin, uploadPhoto.array('image', 1), productImgResize, brandControllers.editBrand)
+router.put('/brand', authMiddleware, isAdmin, uploadPhoto.array('image', 1), brandControllers.editBrand)
 router.delete('/brand/:id', authMiddleware, isAdmin, brandControllers.deleteBrand)
 
 module.exports = router
--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -4,10 +4,10 @@ const categoryControllers = require('../controllers/categoryControllers')
 const { authMiddleware, isAdmin } = require("../middewares/authMiddleware")
 const { uploadPhoto, productImgResize } = require('../middewares/uploadImg')
 
-router.post('/category', authMiddleware, isAdmin, uploadPhoto.array('image', 1), productImgResize, categoryControllers.createCategory)
+router.post('/category', authMiddleware, isAdmin, uploadPhoto.array('image', 1), categoryControllers.createCategory)
 // router.post('/brands', authMiddleware, isAdmin, categoryControllers.createArrayBrand)
 router.get('/categorys', categoryControllers.getAllCategory)
-router.put('/category', authMiddleware, isAdmin, uploadPhoto.array('image', 1), productImgResize, categoryControllers.editCategory)
+router.put('/category', authMiddleware, isAdmin, uploadPhoto.array('image', 1), categoryControllers.editCategory)
 router.delete('/category/:id', authMiddleware, isAdmin, categoryControllers.deleteCategory)
 
 module.exports = router
